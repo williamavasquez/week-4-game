ce0b9fa1b4ee408a662ab43b7a54891f4f55877d
williamavasquez <william.a.vasquez@gmail.com>
2016-03-02 18:42:08 -0500

    working game - missing max min random number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,16 +7,17 @@ $(document).ready(function () {
 		loss = 0;
 
 	//the computer picks a random number between 1 and 120 and prints it on the screen
-	guessValue = Math.floor(Math.random() * 120);
-	$('#gameValue').html(guessValue)
+	guessValue = Math.floor(Math.random() * (120-19+ 1) + 19);
+	$('#gameValue').html(guessValue);
 
 // the computer picks a random number for the gems
 	cred = (Math.floor(Math.random() * 12));
 	cblue = Math.floor(Math.random() * 12);
 	cyellow = Math.floor(Math.random() * 12);
 	cgreen = Math.floor(Math.random() * 12);
+	var valueAdded = 0;
 
-	total_score= 0;
+	totalScore= 0;
 	// all the variables are added into an array for easier calling
 	gems = [cblue,cred,cgreen,cyellow];
 
@@ -26,32 +27,41 @@ $(document).ready(function () {
 		console.log("yellow Value " + cyellow);
 
 		console.log("computer guess "+guessValue);
-		console.log("total score "+total_score);
+		console.log("total score "+totalScore);
 		console.log(wins)
 		console.log(loss)
 
 	// when the player clicks a gem they will add this value to the score 
-	$(".gems").click(gemSelect)
+	$(".gems").click(CheckValue);
 
+function CheckValue(){
+// This grabs the value of the gem you picked
+	valueAdded = ($(this).data('gem'));
+	valueAdded = parseInt(valueAdded);
+	valueAdded = gems[valueAdded]
 
-	function gemSelect() {
-		valueAdded = ($(this).data('gem'));
-		valueAdded = parseInt(valueAdded);
-		valueAdded = gems[valueAdded]
-		console.log(valueAdded+valueAdded);
+// adds the variable and imputs it to your total score
+		totalScore = totalScore+valueAdded;
+		$(your_score).html(totalScore);
 
-		if (valueAdded ==guessValue) {
-			wins++;
-			alert("You won!");
+// here we evaluate the result of each click, if we are over, under or equal to what the computer chose
+		if (totalScore>guessValue){
+			loss++;
+			$("#loss").html("Losses: "+loss)
 			clearEvent();
-		}
-		total_score = total_score+valueAdded;
-		$(your_score).html(total_score);
+			} else if(totalScore==guessValue){
+				wins++;
+				$("#win").html("Win: "+wins);
+				clearEvent();
 	};
+};
 
-
+// This function clears the divs and the variable as well as picks a new number
 	function clearEvent(){
-		total_score=0;
-		guessValue=Math.floor(Math.random() * 120);
+		alert("New GAME");
+		totalScore=0;
+		guessValue=Math.floor(Math.random() * (120 - 19 + 1)) + 19);
+		$('#gameValue').html(guessValue);
+		$('#your_score').html('0');
 	}
 });
